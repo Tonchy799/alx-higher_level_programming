f1eb201db0f8ac9aef4af7d7382b8d646625c94e
Tonchy799 <Tonyh799@gmail.com>
2024-04-10 07:48:52 -0700

    update

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -120,8 +120,10 @@ def recursive_solve(board, row, queens, solutions):
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
             solutions = recursive_solve(tmp_board, row + 1,
-            queens + 1, solutions)
-            return (solutions)
+                                        queens + 1, solutions)
+
+    return (solutions)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
