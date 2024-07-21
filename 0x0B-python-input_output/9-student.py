c974896b7be8cc66f242d130d7c08add55c2968d
Tonchy799 <Tonyh799@gmail.com>
2024-07-21 10:27:34 -0700

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,13 +3,13 @@
 
 
 class Student:
-    """Representation of a student"""
+    """Representation of a Student"""
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """Initializes the Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
         def to_json(self):
-            """Returns a dictionary representation of a Student instance"""
+            """returns a dictionary representation of a Student instance"""
             return self.__dict__
